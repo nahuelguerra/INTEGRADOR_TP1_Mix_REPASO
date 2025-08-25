279734f8b9c02f6aa5f812c32eae079be3bd9a5f
Alaina1313 <alainasabrina13@gmail.com>
2025-08-25 19:56:16 -0300

    cree clase azul

---
+++ src/main/java/com/mycompany/integrador_tp1_mix_repaso/Azul.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.integrador_tp1_mix_repaso;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Azul extends Castor{
+    
+    private double propulsion;
+    private String nombre;
+    
+    public Azul(String nombre, double cola, double velocidad){
+        super(cola, velocidad);
+        this.nombre = nombre;
+    }
+
+    public double getPropulsion() {
+        return propulsion;
+    }
+
+    public void setPropulsion(double propulsion) {
+        this.propulsion = propulsion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    @Override
+    public void nadar(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la propulsion para el ornitorrinco azul.");
+        try{
+            double prop = sc.nextDouble();
+            setPropulsion(prop);
+        }catch(InputMismatchException e){
+            System.out.println("Error de tipo de dato: " + e.getMessage());
+        }
+        System.out.println("El ornitorrinco nada con una propulsion de: " + this.propulsion);
+        System.out.println("La velocidad de nado es: " + this.velocidad);
+    }
+    
+}
