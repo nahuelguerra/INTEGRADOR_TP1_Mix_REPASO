dcef881f3d66d4d61b9efca359b531504aa31ecb
IvanAP96 <alistair.pastrana@gmail.com>
2025-08-25 20:05:29 -0300

    Update Verde.java

--- src/main/java/com/mycompany/integrador_tp1_mix_repaso/Verde.java
+++ src/main/java/com/mycompany/integrador_tp1_mix_repaso/Verde.java
@@ -1,14 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.integrador_tp1_mix_repaso;
+package ej_integrador;
+
+import java.util.Scanner;
 
 /**
  *
- * @author tizia
+ * @author JuanmaPC
  */
-public class Verde {
+public class Verde extends Castor implements MamaPata{
+    
+    private double pico;
+    private double patas;
+    private String nombre;
+    
+    public Verde (String nombre, double pico, double patas, double cola, double velocidad){
+        super(cola, velocidad);
+        this.nombre = nombre;
+        this.pico = pico;
+        this.patas = patas;
+    }
+
+    public double getPico() {
+        return pico;
+    }
+
+    public void setPico(double pico) {
+        this.pico = pico;
+    }
+
+    public double getPatas() {
+        return patas;
+    }
+
+    public void setPatas(double patas) {
+        this.patas = patas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    @Override
+    public void tocarOrgano(){
+        System.out.println("Do-Re-Mi");
+//        Scanner sc = new Scanner(System.in);
+        try {
+            System.in.read();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("Fa-Sol-La-Si");
+    }
+    
+    public void TocarGuitorgan(){
+        tocarGuitarra();
+        tocarOrgano();
+        System.out.println("cuac cuaac…!");
+    }
     
 }
+
